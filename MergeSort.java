610922e727d3ec4cb8405c1ea0e3f8d05b58c8dd
J Wu <jenniferwu46@gmail.com>
2014-07-18 00:01:57 -0700

    Initial commit

---
+++ MergeSort.java
@@ -0,0 +1,22 @@
+public class MergeSort{
+    public static void main(String[] args) {
+        // system entry point
+        // god I don't remember how to code in java
+        System.out.println("Hello World");
+        int[] vec1 = {1, 2, 3};
+        int[] vec2 = {1, 2, 3};
+        int[] merged = merge(vec1, vec2);
+    }
+
+    private static int[] merge(int[] v1, int[] v2) {
+        int i1 = 0; int i2 = 0; int iout = 0;
+        int len1 = v1.length; int len2 = v2.length;
+        int[] out = new int[len1+len2];
+
+        while(i1 < v1.len1 || i2 < v2.len2) {
+        }
+           
+        return out; 
+    }
+
+}
