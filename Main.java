f166b126c2c9af17b0327ff038a4b78d5cdacc4a
J Wu <jenniferwu46@gmail.com>
2014-08-31 18:14:13 +0200

    Updated Quicksort with different ways to select pivot element

--- Main.java
+++ Main.java
@@ -1,13 +1,31 @@
 import sort.*;
 import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 public class Main{
     public static void main(String args[]) {
         System.out.println("Hello World");
-        int[] arr = {6,6,6,3,2,2};
+        int[] arr = getIntegerFromFile("intQuickSort.txt");
 
         //MergeSort sorter = new MergeSort(arr);
         QuickSort sorter = new QuickSort(arr);
         sorter.sort();
-        System.out.println(Arrays.toString(sorter.getSortedArray()));
+        //System.out.println(Arrays.toString(sorter.getSortedArray()));
+    }
+
+    private static int[] getIntegerFromFile(String filename){
+        Scanner sc = null;
+        int n = 10000; int i = 0;
+        int[] arr = new int[n];
+        try{ 
+            sc = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.err.println("FileNotFoundException: " + e.getMessage());
+        }
+
+        while (sc != null && sc.hasNextInt()){
+            arr[i++] = sc.nextInt();
+        }
+        return arr;
     }
 }
--- sort/QuickSort.java
+++ sort/QuickSort.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class QuickSort{
     private int[] v;
-    public int nIter;
+    public long nIter;
 
     public QuickSort(int[] arr) {
         v = arr;
     }
     public void sort(){
         nIter = quickSort(v, 0, v.length);
+        System.out.format("QuickSort made %d comparisons\n", nIter);
     }
 
     public void display(){
@@ -36,13 +37,17 @@ public class QuickSort{
             return 0;
         }
         // TODO: implement different ways to choose pivot - currently is first element
-        int pivotIndex = init;
+        int pivotIndex = getPivotIndex(v,init,len,3);
         int pivot = v[pivotIndex];
+//        System.out.println("Beginning of quicksort");
+//        System.out.format("init=%d, len=%d, position %d is pivot\n", init, len, pivotIndex);
+//        System.out.println(Arrays.toString(v));
 
         // put pivot as first element
         v[pivotIndex] = v[init];
         v[init] = pivot;
-
+//        System.out.println("After putting pivot first");
+//        System.out.println(Arrays.toString(v));
 
         int i = init + 1; // i is index of first element larger than pivot
         int temp;
@@ -58,9 +63,52 @@ public class QuickSort{
         // put pivot in the middle
         v[init] = v[i-1];
         v[i-1] = pivot;
-        
+//        System.out.println("End of quicksort:");
+//        System.out.println(Arrays.toString(v));
+//        System.out.format("Recursive calls with %d,%d and %d,%d\n", init, i-init, i, len-(i-init));
         // do the recursive calls
-        return quickSort(v, init, i-init) + quickSort(v, i, len-(i-init));
+        return len-1+ quickSort(v, init, i-init-1) + quickSort(v, i, len-(i-init));
+//        return 0;
             
+    }
+    private static int getPivotIndex(int[] v, int init, int len, int pivotMethod) {
+        /* pivotMethod is an integer
+         * 1 - first element of subarray
+         * 2 - last element of subarray
+         * 3 - median method
+         */
+        switch (pivotMethod) {
+            case 1:
+                return init;
+            case 2:
+                return init+len-1;
+            case 3:
+                // choose middle of 3
+                int a = init;
+                int b = init+len/2-1+len%2;
+                int c = init+len-1;
+                System.out.format("Choosing between {%d, %d, %d}\n", a, b, c);
+                if (v[a] > v[b]) {
+                    if (v[b] > v[c]) {
+                        return b;
+                    } else if (v[a] > v[c]) {
+                        return c;
+                    } else {
+                        return a;
+                    }
+                } else {
+                    if (v[a] > v[c]) {
+                        return a;
+                    } else if (v[b] > v[c]) {
+                        return c;
+                    } else {
+                        return b;
+                    }
+
+                }
+
+            default:
+                return 0;
+        }
     }
 }
